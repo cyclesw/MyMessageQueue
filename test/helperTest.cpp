14849649344eb7e68d2219611c78c851cc52a7e7
catianri <1939653182@qq.com>
2024-06-23 13:26:29 +0800

    update

--- test/helperTest.cpp
+++ test/helperTest.cpp
@@ -1,17 +1,31 @@
-//
-// Created by lang liu on 2024/6/22.
-//
-
-#define BOOST_TEST_MODULE MyTestModule
 #include "help.hpp"
-#include <boost/test/included/unit_test.hpp>
-#include <boost/smart_ptr.hpp>
-#include <boost/scoped_ptr.hpp>
+#include <gtest/gtest.h>
 
-BOOST_AUTO_TEST_SUITE(smart_ptr)
-
-BOOST_AUTO_TEST_CASE(StrHelper)
+class HelperTest : public testing::Test
 {
+
+};
+
+int main()
+{
+    testing::InitGoogleTest();
+    return RUN_ALL_TESTS(); 
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+// //
+// // Created by lang liu on 2024/6/22.
+// //
+
+// #define BOOST_TEST_MODULE MyTestModule
+// #include "help.hpp"
+// #include <boost/test/included/unit_test.hpp>
+// #include <boost/smart_ptr.hpp>
+// #include <boost/scoped_ptr.hpp>
+
+// BOOST_AUTO_TEST_SUITE(smart_ptr)
+
+// BOOST_AUTO_TEST_CASE(StrHelper)
+// {
+// }
+
+// BOOST_AUTO_TEST_SUITE_END()
